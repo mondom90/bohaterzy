db86785c00364d7737c37dc6b81f415cb52318f8
MandS <monika.domaradzkaa@gmail.com>
2022-11-24 18:59:18 +0000

    heros

---
+++ src/Hero.java
@@ -0,0 +1,16 @@
+public class Hero {
+        String name;
+        String abilities;
+        int lvl;
+        double damage;
+        boolean fame;
+
+        Hero(String name, String abilities, int lvl, double damage, boolean fame){
+            this.name=name;
+                this.abilities=abilities;
+                    this.lvl=lvl;
+                            this.damage=damage;
+                                    this.fame=fame;
+
+        }
+    }
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Hero hero1=new Hero("Acer", "villain", 2, 22.5, true);
+        System.out.println();
+    }
+}
